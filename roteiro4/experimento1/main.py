aa8afc456373b45ac4cb468406094558a964aea8
Julio Nunes Avelar <jn5.guape@gmail.com>
2025-09-17 08:59:20 -0300

    Atualizando formato do experimento

---
+++ roteiro4/experimento1/main.py
@@ -0,0 +1,12 @@
+import time
+
+N = 10_000
+buf = bytearray(N)
+
+t0 = time.ticks_us()
+for i in range(N):
+    buf[i] = i & 0xFF   # escrita em RAM (heap)
+t1 = time.ticks_us()
+
+dt_ms = time.ticks_diff(t1, t0) / 1000
+print("RAM write ({} bytes): {:.3f} ms".format(N, dt_ms))
---
+++ roteiro4/experimento2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main() {
+    stdio_init_all();
+    const int N = 10000;
+    static uint8_t buf[10000];
+
+    absolute_time_t t0 = get_absolute_time();
+    for (int i = 0; i < N; i++) buf[i] = i & 0xFF;
+    absolute_time_t t1 = get_absolute_time();
+
+    int64_t us = absolute_time_diff_us(t0, t1);
+    printf("RAM write (%d bytes): %.3f ms\n", N, us/1000.0);
+
+    while (1) tight_loop_contents();
+}
